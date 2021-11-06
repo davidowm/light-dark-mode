7d969e7eb6b9dfcaae2efd27a61fd9696b2ded21
davidowm <davidowm@gmail.com>
2021-11-05 21:16:13 -0400

    fixed images display

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
@@ -9,9 +10,9 @@ const textBox = document.getElementById('text-box');
 
 //Dark or Light images
 const imageMode = function (color) {
-  image1.src = `img/undraw_proud_coder_${color}.svg`;
-  image2.src = `img/undraw_feeling_proud_${color}.svg`;
-  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+  image1.src = require(`img/undraw_proud_coder_${color}.svg`);
+  image2.src = require(`img/undraw_feeling_proud_${color}.svg`);
+  image3.src = require(`img/undraw_conceptual_idea_${color}.svg`);
 };
 
 const toggleDarkLightMode = function (isDark) {
@@ -30,42 +31,12 @@ const toggleDarkLightMode = function (isDark) {
     ? localStorage.setItem('theme', 'dark')
     : localStorage.setItem('theme', 'light');
 };
-//Dark Mode Styles
-// const darkMode = function () {
-//   nav.style.backgroundColor = "rgba(0 0 0 / 50%)";
-//   textBox.style.backgroundColor = "rgba(255 255 255 /50%)";
-//   toggleIcon.children[0].textContent = "Dark Mode";
-//   toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
-//   imageMode("dark");
-//   localStorage.setItem("theme", "dark");
-//   //toggleIcon.children[1].classList.add("fa-moon");
-//   //image1.src = "img/undraw_proud_coder_dark.svg";
-//   //image2.src = "img/undraw_feeling_proud_dark.svg";
-//   //image3.src = "img/undraw_conceptual_idea_dark.svg";
-// };
-// //Light Mode Styles
-// const lightMode = function () {
-//   nav.style.backgroundColor = "rgba(255 255 255 / 50%)";
-//   textBox.style.backgroundColor = "rgba(0 0 0 /50%)";
-//   toggleIcon.children[0].textContent = "Light Mode";
-//   toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
-//   imageMode("light");
-//   localStorage.setItem("theme", "light");
-//   //toggleIcon.children[1].classList.add("fa-sun");
-//   // image1.src = "img/undraw_proud_coder_light.svg";
-//   // image2.src = "img/undraw_feeling_proud_light.svg";
-//   // image3.src = "img/undraw_conceptual_idea_light.svg";
-// };
-// Switch Theme Dynamically
 const switchTheme = function (event) {
-  //console.log(event.target.checked);
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
-    // darkMode();
     toggleDarkLightMode(true);
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
-    // lightMode();
     toggleDarkLightMode(false);
   }
 };
@@ -77,7 +48,6 @@ if (currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme);
   if (currentTheme === 'dark') {
     toggleSwitch.checked = true;
-    // darkMode();
     toggleDarkLightMode(true);
   }
 }
